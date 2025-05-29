2b5a501f50eef95229c8f5f6603b1aa0fca6c2d2
Muhammad Hassan Afridi <muhdhassan.suffah@gmail.com>
2025-05-29 23:30:31 +0500

    updated index.js

--- api/index.js
+++ api/index.js
@@ -15,31 +15,31 @@ app.use(express.static('public'))
 
 // const allowedOrigins = [process.env.AllowedOrigin1, process.env.AllowedOrigin2];
 const allowedOrigins = [
-  "http://localhost:5173", // for local frontend
-  "https://fronted-food-blog-app.vercel.app" // deployed frontend domain
+    "http://localhost:5173", // for local frontend
+    "https://fronted-food-blog-app.vercel.app" // deployed frontend domain
 ]
 // CORS configuration
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      console.log("❌ CORS blocked for origin:", origin);
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            console.log("❌ CORS blocked for:", origin);
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 };
 
 app.use(cors(corsOptions));
 
-app.use("/",require("../routes/user.js"))
+app.use("/", require("../routes/user.js"))
 app.use('/recipe', require('../routes/recipe.js'));
 
 app.listen(PORT, (err) => {
-  console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port ${PORT}`)
 })
 
 module.exports = app
